c7fde9f7de94ecd749ff8d2672020b59267f7c5b
matusilva <matheus10xy@gmail.com>
2022-01-28 20:59:20 -0300

    implementando contador

---
+++ introducao-javascript/contador/assets/js/scripts.js
@@ -0,0 +1,9 @@
+var currentNumber = document.getElementById('currentNumber');
+
+function increment() {
+    currentNumber.innerHTML++;
+}
+
+function decrement() {
+    currentNumber.innerHTML--;
+}
